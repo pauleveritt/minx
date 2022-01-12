f20d30a076860d01dc0d09d104351969612535ca
Paul Everitt <pauleveritt@me.com>
2022-01-12 16:09:26 -0500

    Quick demo of the problem.

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# The Project
+
+This is a file that in a directory we'd like to watch.
--- minx/app.py
+++ minx/app.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
 
+from minx.watchers import watcher
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    print("On startup with", watcher)
+
+    # The watcher instance is an async iterator. It says it works by:
+    #  "using a threaded executor"
+    # So in theory, the code below should work. But it hangs processing:
+    #   - We never get to the print
+    #   - The server no longer answers requests
+    #   - We don't get to shutdown
+    async for changes in watcher.get():
+        print(changes)
+    print("We never get here")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("On shutdown")
 
 
 @app.get("/")

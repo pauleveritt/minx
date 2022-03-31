c096efaa7ada57acec319f6466a00cda221bfb0f
Paul Everitt <pauleveritt@me.com>
2022-03-31 18:18:46 -0400

    Start putting in watchgod.

--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,9 @@
-.. minx documentation master file, created by
-   sphinx-quickstart on Thu Mar 31 17:47:30 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to minx's documentation!
 ================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
 
 Indices and tables
 ==================
--- minx/app.py
+++ minx/app.py
@@ -1,16 +1,63 @@
 import asyncio
+from time import time
 
 from fastapi import FastAPI
+from sphinx.application import Sphinx
+from sphinx.util.docutils import patch_docutils, docutils_namespace
 from watchgod import awatch
 
 WATCH_DIR = "docs"
 app = FastAPI()
 
 
+class MySphinx(Sphinx):
+    pass
+
+
+def make_app():
+    confdir = sourcedir = "docs"
+    outputdir = doctreedir = "docs/_build"
+    builder = "html"
+    confoverrides = {}
+    status = warning = None
+    freshenv = False
+    warningiserror = False
+    tags = []
+    verbosity = 0
+    jobs = 0
+    keep_going = False
+    force_all = False
+    filenames = []
+    patch_docutils(confdir)
+    docutils_namespace()
+    app2 = MySphinx(
+        srcdir=sourcedir,
+        confdir=confdir,
+        outdir=outputdir,
+        buildername="html",
+        doctreedir=doctreedir,
+    )
+    return app2
+
+
+sphinx_app = make_app()
+
+
+def build_docs():
+    force_all = False
+    filenames = ["docs/index.rst"]
+    sphinx_app.build(force_all, filenames)
+    return sphinx_app.statuscode
+
+
 async def watch_changes():
     print(f"Watching changes in directory: {WATCH_DIR}")
     async for changes in awatch(WATCH_DIR):
         print(changes)
+        start = time()
+        build_docs()
+        elapsed = time() - start
+        print(elapsed * 1000)
 
 
 @app.on_event("startup")
